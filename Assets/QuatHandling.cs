dc0e8efd87c5dac2f3a8240adae7fd23bc11894e
emzoid3000 <emidude@gmail.com>
2020-09-08 15:54:05 +0100

    added quaternions update

---
+++ Assets/QuatHandling.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuatHandling : MonoBehaviour
+{
+    public float x, y, z, w; // xi + yj + zk + w
+}
---
+++ Assets/Quatz.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Quatz : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Behaviour_Pose controllerPose;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
