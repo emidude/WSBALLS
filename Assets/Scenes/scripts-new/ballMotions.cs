75665e571ed7f63e20ae90dad4f065eba3bc8cdc
Emily <emidude@gmail.com>
2022-04-28 19:36:26 +0100

    made test2 with cubes coming out of left hand every 2 seconds, next update so move outwards (along rotation?), also make grid

---
+++ Assets/Scenes/scripts-new/ballMotions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ballMotions : MonoBehaviour
+    {
+    //very bad makes controllers appear far from body
+    public Transform body;
+    public Transform rightHand;
+    public Transform leftHand;
+    public Transform ballPF;
+    private Transform projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //every 0.3 second instantiate a sphere
+        InvokeRepeating("LaunchProjectile", 2.0f, 0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        //let that sphere move away according to rotation of controller (tangent to this)
+    }
+
+    void LaunchProjectile()
+    {
+
+        Transform instance = Instantiate(projectile, rightHand.position, rightHand.rotation);
+    }
+
+
+}
---
+++ Assets/Scenes/scripts-new/tease.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class tease : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Behaviour_Pose controllerPose;
+    public Transform pf;
+    public Transform head;
+    public Transform hand;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controllerPose.transform.parent = head.transform;
+        InvokeRepeating("LaunchProjectile", 2.0f, 0.3f);
+
+        hand = controllerPose.transform;
+
+        Debug.Log("hand transform position = " + hand.position);
+        Debug.Log("hand transform lcoal position = " + hand.localPosition);
+        //Debug.Log("transform veloctity = " + hand.transform.veloc) 
+
+        //Vector3 posL = controllerPose.transform.localPosition;
+       // Vector3 posH = head.transform.localPosition;
+
+        Vector3 velo = controllerPose.GetVelocity();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LaunchProjectile()
+    {
+        Transform instance = Instantiate(pf, hand.position, hand.rotation);
+    }
+}
