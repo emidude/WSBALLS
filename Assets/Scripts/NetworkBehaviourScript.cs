998ed68d02d7741240acba2a7d4edc7dc559bb93
Emily <emidude@gmail.com>
2022-07-25 04:03:55 +0100

    added networking scene to run through example, was getting booged down and complicated so decided to go back and build on toms code...

---
+++ Assets/Scripts/NetworkBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Valve.VR;
+
+public class NetworkBehaviourScript : NetworkBehaviour
+
+{
+    public SteamVR_Behaviour_Pose controllerPoseLeft;
+    public GameObject networkCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;        }
+
+        CmdNetworkCubeSpawn();
+    }
+
+    [Command]
+    void CmdNetworkCubeSpawn()
+    {
+        Vector3 handVelocity = controllerPoseLeft.GetVelocity();
+        GameObject cubeClone = (GameObject)Instantiate(networkCube, handVelocity, Quaternion.identity);
+        NetworkServer.Spawn(cubeClone);
+        Destroy(cubeClone, 2.0f);
+    }
+}
