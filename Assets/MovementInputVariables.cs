b6741308780dabb87e4ff587cb0a2ca24b8b5ad9
Emily <emidude@gmail.com>
2022-08-04 17:41:00 +0100

    prev commit, forgot to add changes

--- Assets/MovementInputVariables.cs
+++ Assets/MovementInputVariables.cs
@@ -14,9 +14,6 @@ public class MovementInputVariables : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        localPosX_L = controllerPoseL.transform.localPosition.x;
-        localPosX_R = controllerPoseR.transform.localPosition.x;
-        localPosY_L = controllerPoseL.transform.localPosition.y;
 
     }
     // Update is called once per frame
@@ -24,5 +21,8 @@ public class MovementInputVariables : MonoBehaviour
     {
         //floats do not update with controllers!
         Debug.Log("local pos x L =" + localPosX_L + "controller=" + controllerPoseL.transform.localPosition.x );
+        float t = Time.time;
+        transform.localPosition = new Vector3(Mathf.Sin(  controllerPoseR.GetVelocity().x + t), Mathf.Sin(controllerPoseR.GetVelocity().y + t), Mathf.Sin(controllerPoseR.GetVelocity().z + t)) ;
+
     }
 }
